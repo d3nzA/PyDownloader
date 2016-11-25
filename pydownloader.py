132c18684049fe719fa24c7c340371c3bd20d5b6
DenzAlovera <denz.alovera@gmail.com>
2016-11-25 14:25:19 +0800

    enhanced file separator

--- pydownloader.py
+++ pydownloader.py
@@ -44,7 +44,7 @@ class Downloader(QDialog):
     def browse_file(self):
         save_file = QFileDialog.getSaveFileName(self, caption="Save File As",
                                                 directory=".", filter="All Files(*.*)")
-        self.save_location.setText(save_file)
+        self.save_location.setText(QDir.toNativeSeparators(save_file))
 
     def download(self):
         url = self.url.text()
